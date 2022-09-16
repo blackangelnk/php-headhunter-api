8d83237a5d47e6284d4bef078e44712b1e235d7d
Sergey Solovev <ss@vcv.ru>
2022-09-16 14:09:04 +0300

    Manager resume limits

--- src/EndPoints/Manager.php
+++ src/EndPoints/Manager.php
@@ -17,16 +17,7 @@ class Manager extends Endpoint
      */
     public function preferences($managerId = null)
     {
-        $employerId = $this->getCurrentEmployerId();
-        $managerId = $managerId ?: $this->getCurrentManagerId();
-
-        $uri = str_replace(
-            ['{employer_id}', '{manager_id}'],
-            [$employerId, $managerId],
-            '{employer_id}/managers/{manager_id}/settings'
-        );
-
-        return $this->getResource($uri);
+        return $this->getManagerResource('settings', $managerId);
     }
 
     /**
@@ -36,13 +27,33 @@ class Manager extends Endpoint
      */
     public function access($managerId = null)
     {
+        return $this->getManagerResource('method_access', $managerId);
+    }
+
+    /**
+     * Get manager method access
+     * @param integer $managerId
+     * @return array
+     */
+    public function resumeLimits($managerId = null)
+    {
+        return $this->getManagerResource('limits/resume', $managerId);
+    }
+
+    /**
+     * @param string $resource
+     * @param integer $managerId
+     * @return array
+     */
+    private function getManagerResource(string $resource, $managerId = null)
+    {
         $employerId = $this->getCurrentEmployerId();
         $managerId = $managerId ?: $this->getCurrentManagerId();
 
         $uri = str_replace(
             ['{employer_id}', '{manager_id}'],
             [$employerId, $managerId],
-            '{employer_id}/managers/{manager_id}/method_access'
+            '{employer_id}/managers/{manager_id}/'.$resource
         );
 
         return $this->getResource($uri);
